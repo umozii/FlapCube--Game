8ac7ba82f049a41d9e20e5f962b2f8893ee139b6
umozii <mn4326098@gmail.com>
2025-01-11 18:39:15 +0800

    Update FlapCube!.js

--- FlapCube!.js
+++ FlapCube!.js
@@ -14,7 +14,7 @@ const JUMP = -11.5; // Jump velocity
 const PIPE_WIDTH = 70; // Pipe width
 const MIN_PIPE_GAP = 230; // Minimum vertical gap between pipes
 const MAX_PIPE_GAP = 300; // Maximum vertical gap between pipes
-const MAX_FALL_SPEED = 5; // Maximum fall speed
+const MAX_FALL_SPEED = 4.5; // Maximum fall speed
 const PIPE_SPAWN_INTERVAL = 95; // Frames between pipe spawns
 
 // Game state
